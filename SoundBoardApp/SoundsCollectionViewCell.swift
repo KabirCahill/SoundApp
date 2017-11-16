42cd07805198c5fa43cc409ff91335ef6ec9f39a
KabirCahill <kcahill4@my.bcit.ca>
2017-11-15 22:28:14 -0800

    Initial commit

---
+++ SoundBoardApp/SoundsCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SoundsCollectionViewCell.swift
+//  SoundBoardApp
+//
+//  Created by Kabir Cahill on 2017-11-15.
+//  Copyright © 2017 Kabir Cahill. All rights reserved.
+//
+
+import UIKit
+
+class SoundsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var btn: UILabel!
+}
