14dd5e5c23a2060f20b88eaf0cdd6177d06cda3c
Kabir Cahill <kcahill4@my.bcit.ca>
2017-11-17 10:56:03 -0800

    Cleaned up  record view UI

--- SoundBoardApp/RecordViewController.swift
+++ SoundBoardApp/RecordViewController.swift
@@ -14,15 +14,21 @@ class RecordViewController: UIViewController {
     var seconds = 0
     var timer = Timer()
     @IBOutlet var recordButton: UIButton!
+    @IBOutlet var playButton: UIButton!
     @IBOutlet var timeDisplay: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         recordButton.layer.cornerRadius = 8
-        recordStart()
-        timeDisplay.text = "0:00"
+        recordButtonStart()
+        //timeDisplay.text = ":00"
         isRecording = false
+        
+        playButton.layer.cornerRadius = 8
+        playButton.backgroundColor = UIColor.green
+        playButton.setTitleColor(UIColor.white, for: .normal)
+        playButton.isHidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,24 +39,30 @@ class RecordViewController: UIViewController {
     @IBAction func record(_ sender: Any) {
         if(!isRecording) {
             isRecording = true
-            recordStop()
+            recordButtonStop()
             timer.invalidate()
             timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.updateTimeDisplay), userInfo: nil, repeats: true)
+            timeDisplay.text = ":00"
+            seconds = 0
+            playButton.isHidden = true
 
+            
         } else {
             isRecording = false
-            recordStart()
+            recordButtonStart()
             timer.invalidate()
+            recordButton.setTitle("Record Again", for: .normal)
+            playButton.isHidden = false
         }
     }
     
     @objc func updateTimeDisplay() {
         seconds += 1;
         
-        timeDisplay.text = (seconds < 10 ? "0:0\(seconds)" : "0:\(seconds)")
+        timeDisplay.text = (seconds < 10 ? ":0\(seconds)" : ":\(seconds)")
     }
     
-    func recordStart() {
+    func recordButtonStart() {
         recordButton.layer.borderColor = UIColor.blue.cgColor
         recordButton.layer.borderWidth = 1
         recordButton.backgroundColor = UIColor.white
@@ -58,7 +70,7 @@ class RecordViewController: UIViewController {
         recordButton.setTitle("Start Recording", for: .normal)
     }
     
-    func recordStop() {
+    func recordButtonStop() {
         recordButton.layer.borderWidth = 0
         recordButton.backgroundColor = UIColor.red
         recordButton.setTitleColor(UIColor.white, for: .normal)
